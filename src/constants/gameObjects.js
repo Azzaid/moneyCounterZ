b5b1141b35c54408bd468ad80ca3463d43d3b716
Azzaid <johanas.azzaid@gmail.com>
2019-05-05 07:29:02 +0300

    Start step stop simulation buttons

--- src/constants/gameObjects.js
+++ src/constants/gameObjects.js
@@ -4,9 +4,17 @@ export const GROUND = {
 };
 
 export const JOINT = {
-  bodyRadius:30,
+  bodyRadius:15,
   visualRadius:40,
   friction:0.1,
   collisionBitset:0x0010,
   collisionBitmask:0x0001,
+};
+
+export const RENDERER = {
+  options:{
+    width: 800,
+    height: 800,
+    wireframes: false,
+  }
 };
--- src/engine/index.js
+++ src/engine/index.js
@@ -1,25 +1,48 @@
 import Matter from "matter-js";
 import { getLineLenght, getAngleBetweenDots } from "./math/planimetry.ts"
-import { GROUND } from "../constants/gameObjects"
+import { GROUND, RENDERER } from "../constants/gameObjects"
 import Joint from "./components/joint.js"
 
 export default class Engine {
-    constructor (DOMObj) {
-        this.domObjectRef = DOMObj;
-        this.engine = Matter.Engine.create();
-        this.gameCanvas = document.createElement("canvas");
-        this.gameCanvas.addEventListener('click', event => {this.addJoint(event.clientX - this.gamefieldCornerX, event.clientY - this.gamefieldCornerY)});
-        this.domObjectRef.appendChild(this.gameCanvas);
-        this.renderer = Matter.Render.create({canvas:this.gameCanvas, engine:this.engine});
-        Matter.Render.run(this.renderer);
-        this.updateGamefieldPosition();
-        this.addGround([{x:0,y:400}, {x:300, y:400}, {x:500,y:450}, {x:600,y:450}, {x:800,y:400}]);
+    constructor (gameFieldDOMObj, buttonsWrapperDOMObj) {
+      this.gameFieldWrapper = gameFieldDOMObj;
+      this.gameCanvas = document.createElement("canvas");
+      this.gameCanvas.addEventListener('click', event => {this.addJoint(event.clientX - this.gamefieldCornerX, event.clientY - this.gamefieldCornerY)});
+      this.gameFieldWrapper.appendChild(this.gameCanvas);
+
+      this.buttonsWrapper = buttonsWrapperDOMObj;
+      let startButton = document.createElement("div");
+      startButton.classList.add('button', 'button_start');
+      startButton.innerText = "start";
+      startButton.addEventListener('click', () => {this.startSimulation()});
+      this.buttonsWrapper.appendChild(startButton);
+
+      let stepButton = document.createElement("div");
+      stepButton.classList.add('button', 'button_step');
+      stepButton.innerText = "step";
+      stepButton.addEventListener('click', () => {this.startSimulation(1000)});
+      this.buttonsWrapper.appendChild(stepButton);
+
+      let stopButton = document.createElement("div");
+      stopButton.classList.add('button', 'button_stop');
+      stopButton.innerText = "stop";
+      stopButton.addEventListener('click', () => {this.stopSimulation()});
+      this.buttonsWrapper.appendChild(stopButton);
+
+      this.engine = Matter.Engine.create();
+
+      this.renderer = Matter.Render.create({canvas:this.gameCanvas, engine:this.engine, options:RENDERER.options});
+      Matter.Render.run(this.renderer);
+
+      this.updateGamefieldPosition();
+      this.addGround([{x:0,y:400}, {x:200, y:700}, {x:300,y:700}, {x:350,y:650}, {x:400,y:600}, {x:500,y:550},
+        {x:500,y:700}, {x:700,y:700}, {x:700,y:500}, {x:800,y:500}]);
 
       this.jointsList = [];
     }
 
     updateGamefieldPosition() {
-        const gamefieldCorner = this.domObjectRef.getBoundingClientRect();
+        const gamefieldCorner = this.gameCanvas.getBoundingClientRect();
         this.gamefieldCornerX = gamefieldCorner.left;
         this.gamefieldCornerY = gamefieldCorner.top;
     };
@@ -52,5 +75,29 @@ export default class Engine {
 
   addJoint(x,y) {
     this.jointsList.push(new Joint(x, y, this.engine));
+  };
+
+  startSimulation(time) {
+    console.debug('start');
+    const stepTime = isFinite(time) ? time : false;
+    const startTime = new Date().getTime();
+    this.renderStep(startTime, startTime, stepTime)
+  };
+
+  stopSimulation() {
+    console.debug('stop');
+    this.stopAnimationFlag = true;
+  };
+
+  renderStep(animationStartTime, previousCallTime, targetAnimationLength)  {
+    const currentTime = new Date().getTime();
+    Matter.Engine.update(this.engine, previousCallTime-currentTime, 1);
+    if (!this.stopAnimationFlag) {
+      if (!targetAnimationLength || currentTime - animationStartTime < targetAnimationLength) window.requestAnimationFrame(()=>{
+        this.renderStep(animationStartTime, currentTime, targetAnimationLength)
+      })
+    } else {
+      this.stopAnimationFlag = false;
+    }
   };
 }
